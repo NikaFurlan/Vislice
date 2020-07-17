b870377cf3f874ee6eaf78de4d0bd779c20a27ba
Nika Furlan <akin.nalruf@gmail.com>
2020-07-17 13:53:00 +0200

    preuredila readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#VISLICE
+##VISLICE \n
 Projekt pri vajah iz predmeta Uvod v Programiranje
--- vislice.py
+++ vislice.py
@@ -1,34 +1,53 @@
 import bottle
-
 import model
 
-vislice = model.Vislice()
+SKRIVNOST = 'moja_prva_skrivnost'
+DATOTEKA_S_STANJEM = 'stanje.json'
+DATOTEKA_Z_BESEDAMI = 'besede.txt'
+
+vislice = model.Vislice(DATOTEKA_S_STANJEM, DATOTEKA_Z_BESEDAMI)
+vislice.nalozi_igre_iz_datoteke()
+
 
 @bottle.get('/')
 def index():
-    return bottle.template('datoteke/views/index.tpl')
+    return bottle.template('index.tpl')
+
+
+@bottle.post('/nova_igra/')
+def nova_igra():
+    id_igre = vislice.nova_igra()
+    bottle.response.set_cookie('idigre', 'idigre{}'.format(id_igre), secret=SKRIVNOST, path='/')
+    bottle.redirect('/igra/')
+
+
+@bottle.get('/igra/')
+def pokazi_igro():
+    id_igre = int(bottle.request.get_cookie('idigre', secret=SKRIVNOST).split('e')[1])
+    igra, poskus = vislice.igre[id_igre]
+    return bottle.template('igra.tpl',
+                           igra=igra,
+                           poskus=poskus)
+
 
 @bottle.post('/igra/')
-#model Vislice naredi novo igro in vrene id
-def nova_igra():
-    id_nove_igre = vislice.nova_igra()
-    #naredi se nam nova igra, sedaj želimo novo igro igrati, želimo da nas direktno pošlje na igranje
-    bottle.redirect(f'/igra/{id_nove_igre}/')
-
-
-@bottle.get('/igra/<id_igre:int>/')
-def pokazi_igro(id_igre):
-    igra, poskus = vislice.igre[id_igre]
-
-    return bottle.template('datoteke/views/igra.tpl', igra=igra, poskus=poskus, id_igre=id_igre)
-
-@bottle.post('/igra/<id_igre:int>/')
-def ugibaj(id_igre):
-    #dobim crko 
+def ugibaj():
+    id_igre = int(bottle.request.get_cookie('idigre', secret=SKRIVNOST).split('e')[1])
     crka = bottle.request.forms.getunicode('crka')
-
     vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra/')
 
-    bottle.redirect(f'/igra/{id_igre}/')
+
+@bottle.get('/statistika/')
+def pokazi_statistiko():
+    slovar_statistik = model.statistika(DATOTEKA_S_STANJEM)
+    return bottle.template('statistika.tpl',
+                           slovar_statistik=slovar_statistik)
+
+
+@bottle.get('/img/<picture>')
+def serve_pictures(picture):
+    return bottle.static_file(picture, root='img')
+
 
 bottle.run(reloader=True, debug=True)
